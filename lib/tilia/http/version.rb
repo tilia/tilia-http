a8b6d417885b1092d7173d229b236fd7a930ea23
Jakob Sack <mail@jakobsack.de>
2017-09-25 20:36:56 +0200

    Release 4.2.1.1

--- lib/tilia/http/version.rb
+++ lib/tilia/http/version.rb
@@ -3,7 +3,7 @@ module Tilia
     # This class contains the version number for the HTTP package
     class Version
       # Full version number
-      VERSION = '4.2.1'.freeze
+      VERSION = '4.2.1.1'.freeze
     end
   end
 end
